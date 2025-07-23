8c62194f8dc23518620b17dd90d21ca4d10ab600
TimLai666 <tim930102@icloud.com>
2025-07-23 15:51:30 +0800

    新增使用者查詢及停用功能

--- backend/internal/sql/db_operation.go
+++ backend/internal/sql/db_operation.go
@@ -5,3 +5,15 @@ import "sso/internal/sql/models"
 func AddUser(user *models.User) error {
 	return db.Create(user).Error
 }
+
+func GetUserByID(userID uint) (*models.User, error) {
+	var user models.User
+	if err := db.First(&user, userID).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func SuspendUser(userID uint) error {
+	return db.Model(&models.User{}).Where("id = ?", userID).Update("is_active", false).Error
+}
